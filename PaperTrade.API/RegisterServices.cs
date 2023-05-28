7ee41e2a8a0afc964ca972cfd9340af13a139ee5
= <sdkauk@gmail.com>
2023-05-28 11:32:32 -0500

    Moved dependency injection into its own class and then added Dbconnection to it

---
+++ PaperTrade.API/RegisterServices.cs
@@ -0,0 +1,18 @@
+﻿using PaperTrade.DataAccess;
+
+namespace PaperTrade.API
+{
+    public static class RegisterServices
+    {
+        public static void ConfigureServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddSingleton<IDbConnection, DbConnection>();
+        }
+
+    }
+}
